982715c688498a6ffdf4ef8e08a0a309134c9f06
Javier <jmartin@grupoloyal.com>
2021-02-02 13:15:25 +0100

    Soluciones

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ Cuaquleir tetxo se pduee leer... si se respetan la primera y la última letra de
 
 - Crear una función que devuelva un numero aleatorio (Math.random()) dentro del rango dado.
 - Adivina el Número, generar un número entre el 0 y el 100, introducir un número e informar si es igual, mayor o menor. Hay un máximo de 10 intentos para encontrar el número que sea igual.
-- Crear una función que devuelva un determinado número de números primos.
 - Crear una función que devuelva un array con el numero de elementos indicado, inicializados al valor suministrado.
+- Crear una función que devuelva un determinado número de números primos.
---
+++ js/ejercicios.js
@@ -0,0 +1,69 @@
+function aleatorio(inicio, fin) {
+    if (!inicio) {
+        inicio = 0;
+    }
+    if (!fin) {
+        fin = 100;
+    }
+    return Math.floor(Math.random() * fin) + inicio;
+}
+
+function JuegoDelNumero() {
+    var numeroBuscado = aleatorio(1, 100);
+    var numeroIntroducido;
+    var intentos = 0;
+    var encontrado = false;
+    do {
+        intentos += 1;
+        numeroIntroducido = +prompt('Dame tu numero (' + intentos + ' de 10):');
+        if (numeroBuscado == numeroIntroducido) {
+            encontrado = true;
+        } else if (numeroBuscado > numeroIntroducido) {
+            alert('Mi número es mayor.');
+        } else {
+            alert('Mi número es menor.');
+        }
+    } while (intentos < 10 && !encontrado);
+    if (encontrado) {
+        alert('Bieeen!!! Acertaste.');
+    } else {
+        alert('Upsss! Se acabaron los intentos, el número era el ' + numeroBuscado);
+    }
+    return encontrado;
+}
+
+function dameArray(numeroElementos, valorPorDefecto) {
+    var rslt = new Object();
+    for (var ind = 0; ind < numeroElementos; ind++) {
+        rslt[ind] = valorPorDefecto;
+    }
+    return rslt;
+}
+
+function damePrimos(num) {
+    var calculos = 0;
+    var cuantos = +num;
+    var rslt = [];
+    var candidato = 2;
+    while (cuantos) {
+        while (true) {
+            var esPrimo = true;
+            for (var indice in rslt) {
+                calculos++;
+                if (candidato % rslt[indice] == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            candidato++;
+            if (esPrimo) {
+                rslt.push(candidato - 1);
+                break;
+            }
+        }
+        cuantos--;
+    }
+    console.log('Calculos: ' + calculos);
+    return rslt;
+}
